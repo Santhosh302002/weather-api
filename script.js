4beafad97d978c21e179320ddd4749105092404a
Santhosh302002 <l.santhoshkumar30@gmail.com>
2022-10-04 12:56:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// API_KEY for maps api
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
+/**
+ * Retrieve weather data from openweathermap
+ */
+const getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const weatherPromise  = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+  })
+}
+
+/**
+ * Retrieve city input and get the weather data
+ */
+const searchCity = () => {
+  const city = document.getElementById('city-input').value;
+  console.log(city)
+  getWeatherData(city)
+  .then((res)=>{
+    showWeatherData(res);
+  }).catch((error)=>{
+    console.log(error);
+    console.log("Something happend");
+  })
+}
+
+/**
+ * Show the weather data in HTML
+ */
+let type=""
+showWeatherData = (weatherData) => {
+  document.getElementById("city-name").innerText = weatherData.name;
+  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
+  type=weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
+}
+
+const container = document.querySelector(".container");
+const toggle = document.querySelector(".toggle");
+
+toggle.addEventListener("click", () => {
+  container.classList.toggle("dark")
+    ? (toggle.firstElementChild.className = "far fa-moon")
+    : (toggle.firstElementChild.className = "far fa-sun");
+});
+
+// if(type=="Rain")
+// {
+//   document.getElementById("type").innerText =<i class="fa-solid fa-cloud"></i>;
+// }
+
